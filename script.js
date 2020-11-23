595638256fd3ed22db6d150d1d23915188dd9120
Michele Barreto <michelebarreto1@hotmail.com>
2020-11-22 22:46:52 -0300

    Criando calculo do orçamento

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ document.querySelector(".hamburguer").addEventListener("click", () =>   //aqui e
     document.querySelector(".container").classList.toggle("show-menu")
 
 );
+ document.querySelector("#qtde").addEventListener("change", atualizarPreco);
+/* o change vai monitorar tanto se o usuario digitar ou usar as setas da quantidade de paginas*/
+ document.querySelector("#js").addEventListener("change", atualizarPreco);
+ document.querySelector("#layout-sim").addEventListener("change", atualizarPreco);
+ document.querySelector("#layout-nao").addEventListener("change", atualizarPreco);
+ document.querySelector("#prazo").addEventListener("change", function(){
+     const prazo =document.querySelector("#prazo").value;
+     document.querySelector("label[for=prazo]").innerHTML = `Prazo : ${prazo} semanas`;
+     atualizarPreco();
+ });
+
+function atualizarPreco(){
+ const qtde = document.querySelector("#qtde").value;
+ const temJS= document.querySelector("#js").checked;
+ const incluiLayout =  document.querySelector("#layout-sim").checked;/** o checked verifica se é verdadeiro ou falso */
+ const prazo = document.querySelector("#prazo").value;
+ let preco = qtde * 100;
+ if(temJS) preco = preco + (preco *10/100);/** aqui esta fazendo o calculo para acrescentar 10 % no valor  */
+     /**podemos usar também preco *=1.1 para calcular os 10 % */
+ if(incluiLayout){
+     preco = preco +500;
+ }
+ let taxaUrgencia = 1 - prazo*0.1;/** aqui é adicona o valor se o for urgente acrescenta 10 % */
+ preco = preco + (preco * taxaUrgencia);
+  document.querySelector("#preco").innerHTML= `R$ ${preco.toFixed(2)}`;
+
+
+}
